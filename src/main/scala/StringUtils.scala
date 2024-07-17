7e8a9c058a3d415cd03317c23c4a49b79276a9a5
Alvin Alexander <al@alvinalexander.com>
2024-07-17 12:35:08 -0600

    Added utilities from recent project.

--- src/main/scala/StringUtils.scala
+++ src/main/scala/StringUtils.scala
@@ -7,6 +7,10 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
+
+/**
+ * TODO: Some (many) of these functions need unit tests.
+ */
 object StringUtils {
 
     /**
@@ -300,5 +304,60 @@ object StringUtils {
             default
         }
 
+    /**
+     * Expects a one-line string as input, and returns a two-line string
+     * as output.
+     * @param s Like "Hi there."
+     * @return A string like "Hi there.\n---------".
+     */
+    def addAsciiUnderlineToString(s: String): String = {
+        val underline = "-" * s.length
+        s"$s\n$underline"
+    }
+
+    /**
+     * Formats the given string so it is right-padded to a string
+     * that has the width specified. Ex: Given ("AAPL", 7) as
+     * input, it returns "   AAPL".
+     * @param s The input string.
+     * @param width The desired width. Should be > s.length.
+     * @return A left-padded, right-justified string.
+     */
+    def rightJustify(input: String, width: Int): String =
+        String.format(s"%${width}s", input)
+
+    /**
+     * Formats the given string so it is left-padded to a string
+     * that has the width specified. Ex: Given ("AAPL", 7) as
+     * input, it returns "AAPL   ".
+     *
+     * @param s The input string.
+     * @param width The desired width. Should be > s.length.
+     * @return A right-padded, left-justified string.
+     *
+     * Note that the 'f' interpolator does not work in this case:
+     *     f"$input%${width}s"
+     */
+    def leftJustify(input: String, width: Int): String =
+        String.format(s"%-${width}s", input)
+
+    def removeBlankStrings(strings: Seq[String]) =
+        strings.filterNot(_.trim.equals(""))
+
+    def stringToBoolean(s: String): Boolean =
+        if s.trim.toUpperCase == "TRUE" then true else false
+
+    /**
+     * Merges "123\n456\n789" and "aaa\nbbb\nccc" into
+     * "123aaa\n456bbb\n789ccc".
+     */
+    def mergeMultilineStrings(s1: String, s2: String): String = {
+        val xsLines: Seq[String] = xs.split("\n").toSeq
+        val ysLines: Seq[String] = ys.split("\n").toSeq
+        val zippedLines: Seq[(String, String)] = xsLines.zip(ysLines)
+        val combinedLines: Seq[String] = zippedLines.map { case (x, y) => x + y }
+        combinedLines.mkString("\n")
+    }
+    
 }
 
